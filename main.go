41f1f9455596c4f76ccc2c340d77091356dea72f
“Tiutiunyk <k.tiutiunyk@mmdsmart.com>
2023-06-17 20:31:08 +0300

    get Heals habits

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ import (
 
 type TemplateRenderer struct {
 	templates *template.Template
+	db        *sql.DB
+}
+
+type Habit struct {
+	Name  string
+	Scale int64
 }
 
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, _ echo.Context) error {
@@ -35,11 +41,34 @@ func createDBConnection() (*sql.DB, error) {
 	return db, nil
 }
 
+func getHealthHabits(db *sql.DB) ([]Habit, error) {
+	// Запит до бази даних для отримання значень шкал
+	rows, err := db.Query("SELECT habit_name, scale FROM health_habits ORDER BY habit_name")
+	if err != nil {
+		return nil, err
+	}
+
+	habits := []Habit{}
+
+	for rows.Next() {
+		habit := Habit{}
+
+		err := rows.Scan(&habit.Name, &habit.Scale)
+		if err != nil {
+			return nil, err
+		}
+
+		habits = append(habits, habit)
+	}
+
+	return habits, nil
+}
+
 func main() {
-	// db, err := createDBConnection()
-	//if err != nil {
-	//	return
-	//}
+	db, err := createDBConnection()
+	if err != nil {
+		return
+	}
 
 	e := echo.New()
 
@@ -58,7 +87,25 @@ func main() {
 
 	// Шаблон для сторінки "Health"
 	e.GET("/health", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "health.html", nil)
+		// Отримати значення шкал з бази даних
+		healthHabits, err := getHealthHabits(db)
+		if err != nil {
+			log.Println("Помилка отримання значень шкал:", err)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+		return c.Render(http.StatusOK, "health.html", healthHabits)
+	})
+	e.POST("/health/habits", func(c echo.Context) error {
+		scale := c.QueryParam("scale")
+		habit := c.QueryParam("habit")
+
+		_, err = db.Exec("INSERT INTO health_habits (habit_name, scale) VALUES ($1, $2) ON CONFLICT (habit_name) DO UPDATE SET scale = EXCLUDED.scale", habit, scale)
+		if err != nil {
+			log.Printf("database query error: %s", err)
+			return err
+		}
+
+		return c.NoContent(http.StatusOK)
 	})
 
 	// Шаблон для сторінки "Wishlist"
@@ -71,7 +118,7 @@ func main() {
 		return c.Render(http.StatusOK, "budget.html", nil)
 	})
 
-	err := e.Start(":8080")
+	err = e.Start(":8080")
 	if err != nil {
 		log.Printf("start server: %s", err)
 		return
